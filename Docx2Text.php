52bc6b19ef13e23bfc179dc02f10bfa6174dd45a
陈序潭 <386117932@qq.com>
2018-04-18 15:20:31 +0800

    Update Docx2Text.php

--- Docx2Text.php
+++ Docx2Text.php
@@ -526,13 +526,3 @@ class Docx2Text
         return trim(strip_tags($xml));
     }
 }
-/**
-// 实例化
-$text = new Docx2Text();
-// 加载docx文件
-$text->setDocx('./1.docx');
-// 将内容存入$docx变量中
-$docx = $text->extract();
-// 调试输出
-var_dump($docx);
-*/
